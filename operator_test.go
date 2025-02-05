721e0a702817659b3e5361eb4460de04e2bd32bd
ljqian1990 <ljqian@163.com>
2025-02-05 13:50:24 +0800

    add unit test

---
+++ operator_test.go
@@ -0,0 +1,20 @@
+package rulertest
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestAdd(t *testing.T) {
+	result := Add(1, 2)
+	require.Equal(t, 3, result)
+}
+
+func TestSub(t *testing.T) {
+	result := Sub(2, 1)
+	require.Equal(t, 1, result)
+
+	result = Sub(3, 1)
+	require.Equal(t, 2, result)
+}
